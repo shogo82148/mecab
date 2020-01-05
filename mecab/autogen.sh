6446797fd283cb589680db52fcbae52132b18b98
Ichinose Shogo <shogo82148@gmail.com>
2020-01-06 05:57:26 +0900

    remove libtool

--- mecab/autogen.sh
+++ mecab/autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Running aclocal ..."
+aclocal -I .
+echo "Running autoheader..."
+autoheader
 echo "Running automake ..."
 automake --add-missing --copy
 echo "Running autoreconf ..."
