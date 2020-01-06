19998ee87ab69f84423264cf1c2f4131362992cf
Ichinose Shogo <shogo82148@gmail.com>
2020-01-06 21:41:37 +0900

    rewrite python2 codes to python3

--- mecab/python/setup.py
+++ mecab/python/setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup,Extension,os
-import string
+from distutils.core import setup,Extension
+import subprocess
 
-def cmd1(str):
-    return os.popen(str).readlines()[0][:-1]
+def cmd1(command):
+    return subprocess.check_output(command.split()).decode().rstrip()
 
-def cmd2(str):
-    return string.split (cmd1(str))
+def cmd2(command):
+    return cmd1(command).split()
 
 setup(name = "mecab-python",
-	version = cmd1("mecab-config --version"),
-	py_modules=["MeCab"],
-	ext_modules = [
-		Extension("_MeCab",
-			["MeCab_wrap.cxx",],
-			include_dirs=cmd2("mecab-config --inc-dir"),
-			library_dirs=cmd2("mecab-config --libs-only-L"),
-			libraries=cmd2("mecab-config --libs-only-l"))
-			])
+    version = cmd1("mecab-config --version"),
+    py_modules=["MeCab"],
+    ext_modules = [
+        Extension("_MeCab",
+            ["MeCab_wrap.cxx",],
+            include_dirs=cmd2("mecab-config --inc-dir"),
+            library_dirs=cmd2("mecab-config --libs-only-L"),
+            libraries=cmd2("mecab-config --libs-only-l"))
+        ])
