e3f1cfb29871564dad1e3d13af49b39cee31f4dd
Ichinose Shogo <shogo82148@gmail.com>
2020-01-07 22:21:05 +0900

    use autotools for building Java bindings

---
+++ mecab/java/autogen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Running libtoolize ..."
+libtoolize --force --copy \
+  || glibtoolize --force --copy # fallback for macOS
+echo "Running aclocal ..."
+aclocal -I .
+echo "Running automake ..."
+automake --add-missing --copy
+echo "Running autoconf ..."
+autoconf -i -f
