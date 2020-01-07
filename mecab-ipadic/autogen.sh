2e5f12da05470939c96be5025a8827d8d3dacd59
Ichinose Shogo <shogo82148@gmail.com>
2020-01-07 21:50:20 +0000

    refactor workflow settings (#13)
    * refactor workflow settings
    
    * fix workflow names
    
    * test on macOS
    
    * no need to run ldconfig on darwin
    
    * missing ldconfig
    
    * add CPPFLAGS
    
    * remove generated files by the autotools
    
    * add autogen.sh
    
    * run autotools in tests
    
    * fix autogen.sh
    
    * install autotools on macOS
    
    * fallback to glibtoolize
    
    * remove m4 macros
    
    * use autotools for building Java bindings
    
    * install gettext
    
    * install gettext to all workflows
    
    * brew reinstall gettext
    
    * set ACLOCAL_PATH env
    
    * add setup-darwin.sh
    
    * checkout setup script
    
    * fix java test
    
    * skip java test
    
    * comment out

--- mecab-ipadic/autogen.sh
+++ mecab-ipadic/autogen.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-echo "Running libtoolize ..."
-libtoolize --force --copy \
-  || glibtoolize --force --copy # fallback for macOS
 echo "Running aclocal ..."
 aclocal -I .
-echo "Running autoheader..."
-autoheader
 echo "Running automake ..."
 automake --add-missing --copy
 echo "Running autoconf ..."
--- mecab/java/autogen.sh
+++ mecab/java/autogen.sh
@@ -5,8 +5,6 @@ libtoolize --force --copy \
   || glibtoolize --force --copy # fallback for macOS
 echo "Running aclocal ..."
 aclocal -I .
-echo "Running autoheader..."
-autoheader
 echo "Running automake ..."
 automake --add-missing --copy
 echo "Running autoconf ..."
