f90515e32c444c283bad25d5489d65f0132bb533
Ichinose Shogo <shogo82148@gmail.com>
2020-01-09 01:48:18 +0900

    add update-docs.sh

---
+++ .github/update-docs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eux
+
+ROOT=$(cd "$(dirname "$0")" && cd .. && pwd)
+
+cd "$ROOT/mecab"
+
+# build mecab
+./autogen.sh
+./configure --enable-utf8-only
+make
+
+rm -rf "$ROOT/mecab/doc"/*.html "$ROOT/mecab/doc"/*.png "$ROOT/mecab/doc"/doxygen
+hugo
+
+cd "$ROOT/mecab/doc"
+doxygen mecab.cfg
+make update-html
+
+cd "$ROOT/mecab/man"
+make -C man update-man
+
+cd "$ROOT/mecab"
+make distclean
+
+rm -rf "$ROOT/docs"
+cp -r "$ROOT/mecab/doc" "$ROOT/docs"
