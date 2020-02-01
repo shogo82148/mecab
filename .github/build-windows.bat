418f4696c04a2f53ee8af9d1e0aeff15bd401d89
Ichinose Shogo <shogo82148@gmail.com>
2020-02-01 17:15:51 +0900

    fix path

--- .github/build-windows.bat
+++ .github/build-windows.bat
@@ -5,19 +5,21 @@ nmake -f Makefile.msvc MACHINE=%BUILD_TYPE%
 
 REM build ipadic
 cd ..\..\
-mecab\src\mecab-dict-index.exe -d mecab-ipadic -o dic\ipadic -f EUC-JP -t UTF-8
+mkdir zip\dic\ipadic
+mecab\src\mecab-dict-index.exe -d mecab-ipadic -o zip\dic\ipadic -f EUC-JP -t UTF-8
 
 rem Zip mecab binaries and ipadic
-copy mecab-ipadic\dicrc dic\ipadic\dicrc
-copy mecab-ipadic\COPYING dic\ipadic\COPYING
-copy mecab-ipadic\*.def dic\ipadic\
-copy mecab-ipadic\*.csv dic\ipadic\
-7z a mecab-msvc-%BUILD_TYPE%.zip mecab\src\*.dll
-7z a mecab-msvc-%BUILD_TYPE%.zip mecab\src\*.exe
-7z a mecab-msvc-%BUILD_TYPE%.zip mecab\src\*.lib
-7z a mecab-msvc-%BUILD_TYPE%.zip mecab\src\mecab.h
-7z a mecab-msvc-%BUILD_TYPE%.zip .github\mecabrc
-7z a mecab-msvc-%BUILD_TYPE%.zip dic
-7z a mecab-msvc-%BUILD_TYPE%.zip mecab\BSD
-7z a mecab-msvc-%BUILD_TYPE%.zip mecab\LGPL
-7z a mecab-msvc-%BUILD_TYPE%.zip mecab\GPL
+mkdir zip
+copy mecab-ipadic\dicrc zip\dic\ipadic\dicrc
+copy mecab-ipadic\COPYING zip\dic\ipadic\COPYING
+copy mecab-ipadic\*.def zip\dic\ipadic\
+copy mecab-ipadic\*.csv zip\dic\ipadic\
+copy mecab\src\*.dll zip
+copy mecab\src\*.exe zip
+copy mecab\src\*.lib zip
+copy mecab\BSD zip
+copy mecab\LGPL zip
+copy mecab\src\*.lib zip
+copy mecab\src\mecab.h zip
+copy .github\mecabrc zip
+7z a mecab-msvc-%BUILD_TYPE%.zip zip\*
