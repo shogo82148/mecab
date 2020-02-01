0b793bfcfc84df4936a966d9477988536d3fb8cf
Ichinose Shogo <shogo82148@gmail.com>
2020-02-01 15:05:56 +0900

    fix path

--- .github/build-windows.bat
+++ .github/build-windows.bat
@@ -1,3 +1,3 @@
-cd mecab\src
+cd dist\mecab\src
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %BUILD_TYPE%
 nmake -f Makefile.msvc MACHINE=%BUILD_TYPE%
