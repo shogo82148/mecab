046c301405931e861dc7a76d3e8381497479e658
Ichinose Shogo <shogo82148@gmail.com>
2020-02-02 17:04:45 +0900

    fix broken test

--- mecab/tests/run-eval.sh
+++ mecab/tests/run-eval.sh
@@ -4,7 +4,7 @@
 dir="eval"
 cd "$dir" || exit 1
 ../../src/mecab-system-eval --level="0 1 2 3 4" system answer > test.out
-if --strip-trailing-cr diff test.gld test.out;
+if diff --strip-trailing-cr test.gld test.out;
 then
   : success!
 else
