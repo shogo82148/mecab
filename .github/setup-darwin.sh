46042475ffcaedb573fceb1d38a43219c06d02af
ICHINOSE Shogo <shogo82148@gmail.com>
2025-01-20 23:46:02 +0900

    fix: update setup-darwin.sh to include libiconv flags

--- .github/setup-darwin.sh
+++ .github/setup-darwin.sh
@@ -3,3 +3,5 @@
 set -eux
 brew install automake autoconf libtool gettext libiconv
 echo ACLOCAL_PATH=/usr/local/opt/gettext/share/aclocal/ >> "$GITHUB_ENV"
+echo LDFLAGS="-L/opt/homebrew/opt/libiconv/lib" >> "$GITHUB_ENV"
+echo CPPFLAGS="-I/opt/homebrew/opt/libiconv/include" >> "$GITHUB_ENV"
