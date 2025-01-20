6ed2828fdec6bd34d8c60f1726409d97d8a44344
ICHINOSE Shogo <shogo82148@gmail.com>
2025-01-21 00:04:19 +0900

    add Python3.13 to the build matrix (#125)
    * add Python3.13 to the build matrix
    
    * show dictionary information
    
    * Revert "show dictionary information"
    
    This reverts commit ac4398bdb8de3816cafa24490136549ac0a2645a.
    
    * fix: ensure UTF-8 encoding for parsed output in test.py
    
    * install libiconv
    
    * fix: update setup-darwin.sh to include libiconv flags

--- .github/setup-darwin.sh
+++ .github/setup-darwin.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 set -eux
-brew install automake autoconf libtool gettext
+brew install automake autoconf libtool gettext libiconv
 echo ACLOCAL_PATH=/usr/local/opt/gettext/share/aclocal/ >> "$GITHUB_ENV"
+echo LDFLAGS="-L/opt/homebrew/opt/libiconv/lib" >> "$GITHUB_ENV"
+echo CPPFLAGS="-I/opt/homebrew/opt/libiconv/include" >> "$GITHUB_ENV"
