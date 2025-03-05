a42438b672a6955b1511660bd30e823b6b8936bd
ICHINOSE Shogo <shogo82148@gmail.com>
2025-03-05 22:00:48 +0900

    auditwheel repair

--- .github/build-wheel-many-linux.sh
+++ .github/build-wheel-many-linux.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# build wheels for manylinux
+# based on https://github.com/SamuraiT/mecab-python3/blob/5575170dc71dc5d4e0c79a23c20d71edad5c2845/.github/workflows/entrypoint.sh
 
 set -euxo pipefail
 
@@ -19,4 +21,9 @@ tar xzvf "/mecab/dist/scripts/mecab-python-${MECAB_VERSION}.tar.gz"
 # Build the wheels
 for PYVER in cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311 cp312-cp312 cp313-cp313; do
   "/opt/python/${PYVER}/bin/pip" wheel "/mecab-python-${MECAB_VERSION}" -w /mecab/wheels
+done
+
+# fix the wheels (bundles libs)
+for wheel in /mecab/wheels/*.whl; do
+  auditwheel repair "$wheel" -w /mecab/manylinux-wheels
 done
