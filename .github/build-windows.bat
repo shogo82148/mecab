a2e03dbe092b33431db0b676af831b6dce21d69d
ICHINOSE Shogo <shogo82148@gmail.com>
2025-03-06 20:50:25 +0900

    update build-windows.bat to install and wheel setuptools and pip for Python 3.13

--- .github/build-windows.bat
+++ .github/build-windows.bat
@@ -30,6 +30,8 @@ set PATH=%GITHUB_WORKSPACE%\dist\zip\;%PATH%
 cd %GITHUB_WORKSPACE%\dist\scripts
 copy %GITHUB_WORKSPACE%\dist\mecab\src\*.dll .
 if "%BUILD_TYPE%" == "x64" (
+    py -3.13-64 -m pip install -U setuptools wheel pip
+    py -3.13-64 -m pip wheel .
     py -3.12-64 -m pip install -U setuptools wheel pip
     py -3.12-64 -m pip wheel .
     py -3.11-64 -m pip install -U setuptools wheel pip
@@ -41,6 +43,8 @@ if "%BUILD_TYPE%" == "x64" (
     py -3.8-64 -m pip install -U setuptools wheel pip
     py -3.8-64 -m pip wheel .
 ) else if "%BUILD_TYPE%" == "x86" (
+    py -3.13-32 -m pip install -U setuptools wheel pip
+    py -3.13-32 -m pip wheel .
     py -3.12-32 -m pip install -U setuptools wheel pip
     py -3.12-32 -m pip wheel .
     py -3.11-32 -m pip install -U setuptools wheel pip
