b2cbc8ad22b38f3ed043bf67d73068bd17af9a6d
ICHINOSE Shogo <shogo82148@gmail.com>
2025-03-09 22:48:07 +0900

    update macOS build script to specify target Python versions for wheel building

--- .github/build-wheel-macos.sh
+++ .github/build-wheel-macos.sh
@@ -65,7 +65,6 @@ tar xzvf "dist/scripts/mecab-python-$MECAB_VERSION.tar.gz"
   python -m pip install --upgrade setuptools wheel pip setuptools-scm
   python -m pip install cibuildwheel==2.23.0
 
-  # don't bother with pypy wheels
-  export CIBW_SKIP="pp*"
+  export CIBW_BUILD="cp38-* cp39-* cp310-* cp311-* cp312-* cp313-*"
   python -m cibuildwheel --platform macos --archs x86_64,arm64,universal2 --output-dir ../dist
 )
