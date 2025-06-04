2f99a6c15c69c9e97e9bb9f003b5b8696a156e55
ICHINOSE Shogo <shogo82148@gmail.com>
2025-06-04 20:43:12 +0900

    fix ACLOCAL_PATH

--- .github/setup-darwin.sh
+++ .github/setup-darwin.sh
@@ -2,6 +2,6 @@
 
 set -eux
 brew install automake autoconf libtool gettext libiconv
-echo ACLOCAL_PATH=/usr/local/opt/gettext/share/aclocal/ >> "$GITHUB_ENV"
+echo ACLOCAL_PATH=/opt/homebrew/share/gettext/m4 >> "$GITHUB_ENV"
 echo LDFLAGS="-L/opt/homebrew/opt/libiconv/lib" >> "$GITHUB_ENV"
 echo CPPFLAGS="-I/opt/homebrew/opt/libiconv/include" >> "$GITHUB_ENV"
