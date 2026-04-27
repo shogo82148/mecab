bb1531eb4ad2e35a2700461acb4597facb53e109
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2026-04-27 16:23:18 -0700

    Fix finding AM_ICONV

--- mecab/autogen.sh
+++ mecab/autogen.sh
@@ -4,10 +4,10 @@ echo "Running libtoolize ..."
 libtoolize --force --copy \
   || glibtoolize --force --copy # fallback for macOS
 echo "Running aclocal ..."
-aclocal -I .
+aclocal -I . -I /usr/share/gettext/m4
 echo "Running autoheader..."
 autoheader
 echo "Running automake ..."
 automake --add-missing --copy
 echo "Running autoconf ..."
-autoconf -i -f
+autoconf -i -f -I /usr/share/gettext/m4
